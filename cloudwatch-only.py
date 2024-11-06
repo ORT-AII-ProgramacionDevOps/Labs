fe5fb0787539891616de2a6fb8feed7ad12cc778
gferradas <guillermo.ferradas@fenicio.io>
2024-11-06 11:30:39 -0300

    Agrega un evento de prueba con la fecha de hoy

--- cloudwatch-only.py
+++ cloudwatch-only.py
@@ -1,6 +1,11 @@
 import boto3
+import time
 
 logs=boto3.client('logs')
 
 logs.create_log_group(logGroupName='test-log-group')
 logs.create_log_stream(logGroupName='test-log-group', logStreamName='my-log-stream')
+
+current_time = int(time.time() * 1000)
+
+logs.put_log_events(logGroupName='test-log-group', logStreamName='my-log-stream', logEvents=[{'timestamp': current_time, 'message': 'test log message'}])
