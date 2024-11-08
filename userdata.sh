d0ff21d158d44d64a44b9ebc15bb63e3391f3056
gferradas <guillermo.ferradas@fenicio.io>
2024-11-07 21:51:18 -0300

    Remueve configuracion ssh

--- userdata.sh
+++ userdata.sh
@@ -13,12 +13,6 @@ echo "newuser:password" | sudo chpasswd
 # Add the new user to the sudo group
 sudo usermod -aG sudo newuser
 
-# Set up SSH keys for the new user
-sudo mkdir -p /home/newuser/.ssh
-sudo cp /root/.ssh/authorized_keys /home/newuser/.ssh/
-sudo chown -R newuser:newuser /home/newuser/.ssh
-sudo chmod 700 /home/newuser/.ssh
-sudo chmod 600 /home/newuser/.ssh/authorized_keys
 
 # Clean up
 sudo apt-get autoremove -y
