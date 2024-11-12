2107131e1bce6f440ec25096ba660ae5a79d6c9b
gferradas <guillermo.ferradas@fenicio.io>
2024-11-11 21:19:29 -0300

    Agrega creacion de bucket

--- Respuesta/ejercicio2.py
+++ Respuesta/ejercicio2.py
@@ -1,11 +1,15 @@
 import boto3
 
 bucket_name = 'dwijdisojdaois'
-file_key = 'snicolasg/home/sysadmin/DevOps/python/aws/file.txt'
+file_key = 'script-de-userdata'
 download_path = '/home/gferradas/script-de-userdata'
 
 # Crear un cliente de S3
 s3 = boto3.client('s3')
+
+# Creacion de bucket
+
+s3.create_bucket(Bucket=bucket_name)
 
 # Descargar el archivo
 s3.download_file(bucket_name, file_key, download_path)
