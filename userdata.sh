eec104f365e4cbbd06208ac0d3d84de80283785e
gferradas <guillermo.ferradas@fenicio.io>
2024-11-12 22:36:35 -0300

    Cambia userdata y agrega ejercicio 11 hecho en clase

--- userdata.sh
+++ userdata.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-# Update the package list and install updates
-sudo apt-get update -y
-sudo apt-get upgrade -y
 
-sudo apt-get install -y apache2
-
-# Set up a new user
-sudo useradd -m -s /bin/bash newuser
-echo "newuser:password" | sudo chpasswd
-
-# Add the new user to the sudo group
-sudo usermod -aG sudo newuser
+useradd guillermo.ferradas
 
 
-# Clean up
-sudo apt-get autoremove -y
-sudo apt-get clean
+mkdir /tmp/270809
