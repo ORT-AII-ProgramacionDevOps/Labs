4c013d9611a666d34facf81c0f6e5a7dec3d3486
gferradas <guillermo.ferradas@fenicio.io>
2025-05-13 11:04:48 -0300

    Changes path

--- Ejercicios/boto3/Respuestas/ejercicio2.py
+++ Ejercicios/boto3/Respuestas/ejercicio2.py
@@ -2,7 +2,7 @@ import boto3
 
 bucket_name = 'test-bucket-prog-devops-2025'
 file_key = 'script-de-userdata'
-download_path = '/home/gferradas/script-de-userdata'
+download_path = '/path/to/download/file'
 
 # Crear un cliente de S3
 s3 = boto3.client('s3')
