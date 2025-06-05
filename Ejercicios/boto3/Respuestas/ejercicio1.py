31543715553044aa5dee2f3a4b07e0983f1be4b5
gferradas <guillermo.ferradas@fenicio.io>
2025-06-05 15:28:32 -0300

    Fix typos

--- Ejercicios/boto3/Respuestas/ejercicio1.py
+++ Ejercicios/boto3/Respuestas/ejercicio1.py
@@ -2,9 +2,9 @@ import boto3
 
 s3_client = boto3.client('s3')
 
-file_name = 'path/to/your/file.txt'
+file_name = 'path/to/your/file'
 bucket_name = 'your-bucket-name'
-object_name = file_name  
+object_name = file_name.split('/')[-1]  # Extract the file name from the path  
 try:    
     boto3.client('s3').create_bucket(Bucket='dwijdisojdaois')
     
