d7fc63cc383b34f0fb80d799c73667f769fde036
gferradas <guillermo.ferradas@fenicio.io>
2025-06-05 15:33:58 -0300

    Add exception if occurs an error creating bucket

--- Ejercicios/boto3/Respuestas/ejercicio1.py
+++ Ejercicios/boto3/Respuestas/ejercicio1.py
@@ -6,7 +6,10 @@ file_name = 'path/to/your/file'
 bucket_name = 'your-bucket-name'
 object_name = file_name.split('/')[-1]  # Extract the file name from the path  
 try:    
-    boto3.client('s3').create_bucket(Bucket='dwijdisojdaois')
+    boto3.client('s3').create_bucket(Bucket=bucket_name)
+except boto3.exceptions.S3CreateError as e:
+    print(f"Error creating bucket: {e}")
+    exit(1)
     
     s3_client.upload_file(file_name, bucket_name, object_name)
 
